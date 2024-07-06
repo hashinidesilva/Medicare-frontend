69c4a58c08e45346f85b85c14bfddb505a9afc97
hashinidesilva <hashmdesilva@gmail.com>
2024-07-06 21:05:31 +0530

    * Adding apiBaseUrl

--- public/config.js
+++ public/config.js
@@ -1,3 +1,3 @@
 window.config = {
-  apiBaseUrl: 'http://localhost:8080',
+  apiBaseUrl: 'https://6ea5f359-b5ee-4ced-ad1f-921be1a21925-prod.e1-us-east-azure.choreoapis.dev/medicare/medicare-service/v1.0',
 };
