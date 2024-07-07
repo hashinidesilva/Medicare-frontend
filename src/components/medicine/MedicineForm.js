624cae14b644c604c3aab5502ddd05f6a863502d
hashinidesilva <hashmdesilva@gmail.com>
2024-07-07 20:32:21 +0530

    * Adding more medicine types

--- src/components/medicine/MedicineForm.js
+++ src/components/medicine/MedicineForm.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import {useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 import {
   Box,
@@ -13,9 +13,9 @@ import {
   OutlinedInput,
   Stack,
   TextField,
-  Typography
-} from "@mui/material";
-import useInput from "../../hooks/useInput";
+  Typography,
+} from '@mui/material';
+import useInput from '../../hooks/useInput';
 
 const medicineTypeList = [
   {
@@ -37,7 +37,15 @@ const medicineTypeList = [
   {
     value: 'Drops',
     label: 'Drops',
-  }
+  },
+  {
+    value: 'Injection',
+    label: 'Injection',
+  },
+  {
+    value: 'Packet',
+    label: 'Packet',
+  },
 ];
 
 const MedicineForm = (props) => {
@@ -49,7 +57,7 @@ const MedicineForm = (props) => {
     isValid: nameIsValid,
     hasError: nameHasError,
     valueChangeHandler: nameChangeHandler,
-    inputBlurHandler: nameBlurHandler
+    inputBlurHandler: nameBlurHandler,
   } = useInput(value => value.trim() !== '', '');
 
   const {
@@ -57,7 +65,7 @@ const MedicineForm = (props) => {
     isValid: unitPriceIsValid,
     hasError: unitPriceHasError,
     valueChangeHandler: unitPriceChangeHandler,
-    inputBlurHandler: unitPriceBlurHandler
+    inputBlurHandler: unitPriceBlurHandler,
   } = useInput(value => value > 0, 0);
 
   const {
@@ -65,7 +73,7 @@ const MedicineForm = (props) => {
     isValid: unitsIsValid,
     hasError: unitsHasError,
     valueChangeHandler: unitsChangeHandler,
-    inputBlurHandler: unitsBlurHandler
+    inputBlurHandler: unitsBlurHandler,
   } = useInput(value => value > 0, 0);
 
   const {
@@ -73,7 +81,7 @@ const MedicineForm = (props) => {
     isValid: minUnitsIsValid,
     hasError: minUnitsHasError,
     valueChangeHandler: minUnitsChangeHandler,
-    inputBlurHandler: minUnitsBlurHandler
+    inputBlurHandler: minUnitsBlurHandler,
   } = useInput(value => value > 0, 0);
 
   const {
@@ -81,10 +89,13 @@ const MedicineForm = (props) => {
     isValid: typeIsValid,
     hasError: typeHasError,
     valueChangeHandler: typeChangeHandler,
-    inputBlurHandler: typeBlurHandler
-  } = useInput(value => medicineTypeList.map(item => item.value).includes(value), medicineTypeList[0].value);
+    inputBlurHandler: typeBlurHandler,
+  } = useInput(
+      value => medicineTypeList.map(item => item.value).includes(value),
+      medicineTypeList[0].value);
 
-  const formIsValid = nameIsValid && unitPriceIsValid && unitsIsValid && typeIsValid && minUnitsIsValid;
+  const formIsValid = nameIsValid && unitPriceIsValid && unitsIsValid &&
+      typeIsValid && minUnitsIsValid;
 
   useEffect(() => {
     if (medicine) {
@@ -94,7 +105,12 @@ const MedicineForm = (props) => {
       minUnitsChangeHandler(medicine.minimumUnits);
       typeChangeHandler(medicine.type);
     }
-  }, [medicine, nameChangeHandler, unitPriceChangeHandler, unitsChangeHandler, typeChangeHandler]);
+  }, [
+    medicine,
+    nameChangeHandler,
+    unitPriceChangeHandler,
+    unitsChangeHandler,
+    typeChangeHandler]);
 
   const submitHandler = async (event) => {
     event.preventDefault();
@@ -103,122 +119,127 @@ const MedicineForm = (props) => {
       unitPrice: unitPrice,
       units: units,
       minimumUnits: minUnits,
-      type: type
+      type: type,
     };
 
     props.onAddMedicine(newMedicine);
   };
 
   return (
-    <Box sx={{border: 1, borderColor: "#1e88e5", width: '100%'}}>
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: 'center',
-          height: 40,
-          justifyContent: "flex-start",
-          backgroundColor: "#1e88e5",
-          paddingLeft: '20px'
-        }}>
-        <Typography variant="h5" color="#ffffff">Medicine Form</Typography>
-      </Box>
-      <form onSubmit={submitHandler} style={{margin: '20px'}}>
-        <Grid container spacing={4}>
-          <Grid item xs={12}>
-            <TextField
-              error={nameHasError}
-              fullWidth
-              id="name"
-              label="Medicine name"
-              type="text"
-              value={name}
-              onChange={(event) => nameChangeHandler(event.target.value)}
-              onBlur={nameBlurHandler}
-              helperText={nameHasError && "Name must not be empty"}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              error={typeHasError}
-              fullWidth
-              id="type"
-              select
-              label="Medicine type"
-              value={type || medicineTypeList[0].value}
-              onChange={(event) => typeChangeHandler(event.target.value)}
-              onBlur={typeBlurHandler}
-            >
-              {medicineTypeList.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Grid>
-          <Grid item xs={6}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor="unit-price">Unit Price</InputLabel>
-              <OutlinedInput
-                error={unitPriceHasError}
-                id="unit-price"
-                startAdornment={<InputAdornment position="start">Rs</InputAdornment>}
-                label="Unit Price"
-                value={unitPrice}
-                onChange={(event) => unitPriceChangeHandler(event.target.value)}
-                onBlur={unitPriceBlurHandler}
+      <Box sx={{border: 1, borderColor: '#1e88e5', width: '100%'}}>
+        <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              height: 40,
+              justifyContent: 'flex-start',
+              backgroundColor: '#1e88e5',
+              paddingLeft: '20px',
+            }}>
+          <Typography variant="h5" color="#ffffff">Medicine Form</Typography>
+        </Box>
+        <form onSubmit={submitHandler} style={{margin: '20px'}}>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <TextField
+                  error={nameHasError}
+                  fullWidth
+                  id="name"
+                  label="Medicine name"
+                  type="text"
+                  value={name}
+                  onChange={(event) => nameChangeHandler(event.target.value)}
+                  onBlur={nameBlurHandler}
+                  helperText={nameHasError && 'Name must not be empty'}
               />
-              {unitPriceHasError && <FormHelperText error>Unit price must be > 0</FormHelperText>}
-            </FormControl>
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              error={unitsHasError}
-              fullWidth
-              id="units"
-              label="Units"
-              type="number"
-              value={units}
-              onChange={(event) => unitsChangeHandler(event.target.value)}
-              onBlur={unitsBlurHandler}
-              helperText={unitsHasError && "Units must be > 0"}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              error={minUnitsHasError}
-              fullWidth
-              id="minUnits"
-              label="Minimum Units"
-              type="number"
-              value={minUnits}
-              onChange={(event) => minUnitsChangeHandler(event.target.value)}
-              onBlur={minUnitsBlurHandler}
-              helperText={minUnitsHasError && "Minimum Units must be > 0"}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Stack spacing={2} direction={"row"}>
-              <Button
-                variant="contained"
-                type="submit"
-                disabled={!formIsValid}
-                sx={{backgroundColor: "#0003b2"}}
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                  error={typeHasError}
+                  fullWidth
+                  id="type"
+                  select
+                  label="Medicine type"
+                  value={type || medicineTypeList[0].value}
+                  onChange={(event) => typeChangeHandler(event.target.value)}
+                  onBlur={typeBlurHandler}
               >
-                Save
-              </Button>
-              <Button
-                variant="contained"
-                onClick={() => navigate(-1)}
-                sx={{backgroundColor: "#b25600"}}
-              >
-                Cancel
-              </Button>
-            </Stack>
+                {medicineTypeList.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="unit-price">Unit Price</InputLabel>
+                <OutlinedInput
+                    error={unitPriceHasError}
+                    id="unit-price"
+                    startAdornment={<InputAdornment
+                        position="start">Rs</InputAdornment>}
+                    label="Unit Price"
+                    value={unitPrice}
+                    onChange={(event) => unitPriceChangeHandler(
+                        event.target.value)}
+                    onBlur={unitPriceBlurHandler}
+                />
+                {unitPriceHasError && <FormHelperText error>Unit price must be >
+                  0</FormHelperText>}
+              </FormControl>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                  error={unitsHasError}
+                  fullWidth
+                  id="units"
+                  label="Units"
+                  type="number"
+                  value={units}
+                  onChange={(event) => unitsChangeHandler(event.target.value)}
+                  onBlur={unitsBlurHandler}
+                  helperText={unitsHasError && 'Units must be > 0'}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                  error={minUnitsHasError}
+                  fullWidth
+                  id="minUnits"
+                  label="Minimum Units"
+                  type="number"
+                  value={minUnits}
+                  onChange={(event) => minUnitsChangeHandler(
+                      event.target.value)}
+                  onBlur={minUnitsBlurHandler}
+                  helperText={minUnitsHasError && 'Minimum Units must be > 0'}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Stack spacing={2} direction={'row'}>
+                <Button
+                    variant="contained"
+                    type="submit"
+                    disabled={!formIsValid}
+                    sx={{backgroundColor: '#0003b2'}}
+                >
+                  Save
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={() => navigate(-1)}
+                    sx={{backgroundColor: '#b25600'}}
+                >
+                  Cancel
+                </Button>
+              </Stack>
+            </Grid>
+            {error && <Typography color={'red'}
+                                  sx={{margin: '20px'}}>{error}</Typography>}
           </Grid>
-          {error && <Typography color={"red"} sx={{margin: '20px'}}>{error}</Typography>}
-        </Grid>
-      </form>
-    </Box>
+        </form>
+      </Box>
   );
 };
 
