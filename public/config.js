22da3930bb43395c8a2c598dcd892f77daf97c2a
hashinidesilva <hashmdesilva@gmail.com>
2024-07-18 23:08:58 +0530

    * Revert - Adding development API URL for testing purposes

--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 window.config = {
-  apiBaseUrl: 'https://6ea5f359-b5ee-4ced-ad1f-921be1a21925-dev.e1-us-east-azure.choreoapis.dev/medicare/medicare-service/v1.0',
+  apiBaseUrl: 'https://6ea5f359-b5ee-4ced-ad1f-921be1a21925-prod.e1-us-east-azure.choreoapis.dev/medicare/medicare-service/v1.0',
   // apiBaseUrl: 'http://localhost:8080',
 };
